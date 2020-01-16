09e1290a842300d9bef18114afe404c1e40cfdb0
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:47:45 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,18 @@
-print('hello')
-print('worl
+elements = [['Li', 'Na', 'K'], ['F', 'Cl','Br']]
+for inner_list in  elements:
+    print(inner_list)
+    
+['Li', 'Na', 'K']
+['F', 'Cl', 'Br']
+
+elements = [['Li', 'Na', 'K'], ['F', 'Cl','Br']]
+for inner_list in  elements:
+  for item in inner_list:
+      print(item)
+      
+Li 
+Na
+K
+F
+Cl
+Br
